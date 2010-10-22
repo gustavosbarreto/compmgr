4bd110e6c46103752ecc37ebffe8bacb81f91ef8
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2010-10-22 11:53:32 -0200

    Convert text charset to utf8

--- src/client.h
+++ src/client.h
@@ -1,21 +1,21 @@
 /*
  * $Id$
  *
- * Copyright © 2003-2004 Fredrik Höglund <fredrik@kde.org>
+ * Copyright Â© 2003-2004 Fredrik HÃ¶glund <fredrik@kde.org>
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
  * the above copyright notice appear in all copies and that both that
  * copyright notice and this permission notice appear in supporting
- * documentation, and that the name of Fredrik Höglund not be used in
+ * documentation, and that the name of Fredrik HÃ¶glund not be used in
  * advertising or publicity pertaining to distribution of the software without
- * specific, written prior permission.  Fredrik Höglund makes no
+ * specific, written prior permission.  Fredrik HÃ¶glund makes no
  * representations about the suitability of this software for any purpose.  It
  * is provided "as is" without express or implied warranty.
  *
- * FREDRIK HÖGLUND DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * FREDRIK HÃ–GLUND DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
  * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
- * EVENT SHALL FREDRIK HÖGLUND BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * EVENT SHALL FREDRIK HÃ–GLUND BE LIABLE FOR ANY SPECIAL, INDIRECT OR
  * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
  * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
  * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
